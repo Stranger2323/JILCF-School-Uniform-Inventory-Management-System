3674df156d807a13c617a57f887f49d41d6713a4
Stranger2323 <josiahdeasis009@gmail.com>
2025-02-22 21:23:03 +0800

    Fix static file paths for Render deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import hashlib
 import re
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
 CORS(app)
 
 # Your existing routes and functions here
