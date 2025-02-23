4b4ae998f3595abe6022e4706da377e3212e8ebb
Stranger2323 <josiahdeasis009@gmail.com>
2025-02-23 11:10:03 +0800

    Add innovative tile-based page transition
    - Replace circle transition with sliding tiles
    - Add spinning loader animation
    - Improve transition timing and delays
    - Maintain smooth back/forward navigation
    - Verify all pages working correctly

--- static/js/transitions.js
+++ static/js/transitions.js
@@ -1,6 +1,28 @@
-// Create transition element
+// Create transition element with loading animation
 const transitionElement = document.createElement('div');
 transitionElement.className = 'page-transition';
+
+// Create loading icon
+const iconElement = document.createElement('div');
+iconElement.className = 'page-transition__icon';
+iconElement.innerHTML = `
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+         x="0px" y="0px" width="40px" height="40px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
+        <path opacity="0.2" fill="#fff" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
+        <path fill="#fff" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0C22.32,8.481,24.301,9.057,26.013,10.047z">
+            <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 20 20" to="360 20 20" dur="0.5s" repeatCount="indefinite"/>
+        </path>
+    </svg>
+`;
+transitionElement.appendChild(iconElement);
+
+// Create tiles
+for (let i = 0; i < 5; i++) {
+    const tile = document.createElement('div');
+    tile.className = 'page-transition__tile';
+    transitionElement.appendChild(tile);
+}
+
 document.body.appendChild(transitionElement);
 
 // Handle all link clicks for page transitions
@@ -15,7 +37,7 @@ document.addEventListener('click', (e) => {
         // Navigate to new page after transition
         setTimeout(() => {
             window.location.href = link.href;
-        }, 300);
+        }, 1000); // Adjusted timing to match animation
     }
 });
 
